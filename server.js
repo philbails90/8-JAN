ec5d6d0b10405ebc05318ad78b476c6e62bbefc5
Phil Bailey <phil@Phils-MacBook.local>
2022-01-09 15:11:55 +0000

    linked images

--- server.js
+++ server.js
@@ -4,15 +4,31 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.use("/styles",express.static(__dirname + "/styles"));
+app.use("/styles",express.static(__dirname + "/styles"))
+app.use("/images",express.static(__dirname + "/images"))
+
 
 app.get('/', (req, res) => {
-    const projects = {
+    const projects = [{
         title: 'test project',
         createdAt: Date.now(),
-        description: 'test description'
-    }
-    res.render('index', {project: projects})
+        description: 'test description',
+        image: 'image1.png'
+    },
+        {
+        title: 'test project2',
+        createdAt: Date.now(),
+        description: 'test description',
+        image: 'image1.png'
+    },
+        {
+        title: 'test project3',
+        createdAt: Date.now(),
+        description: 'test description',
+        image: 'image1.png'
+        }
+    ]
+    res.render('index', {projects: projects})
 })
 
 app.listen(5001)
